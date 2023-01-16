df392cf75c62a9fa6e2347e50033dad0df8a848e
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-16 18:50:06 +0600

    done: task6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task6;
-
+import java.lang.Math;
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+    int i = (int) Math.round(numberToBeRounded);
+        System.out.println(i);
     }
 }
